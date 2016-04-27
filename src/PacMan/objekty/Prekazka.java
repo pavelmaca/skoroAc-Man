d9c2a528918ff095041e3da1fa8b03a944764389
Koule7b <stepan.mudra@gamil.com>
2016-04-27 12:23:18 +0200

    prekazka

--- src/PacMan/objekty/Prekazka.java
+++ src/PacMan/objekty/Prekazka.java
@@ -1,7 +1,33 @@
 package PacMan.objekty;
 
+import java.awt.*;
+
 /**
  * Created by Admin on 19.4.2016.
  */
 public class Prekazka {
+    private int x;
+    private int y;
+    private  int sirka;
+    private int vyska;
+    private Color barvaVnitrku;
+    private Color barvaOkraje;
+    public Prekazka(int x, int y, int sirka, int vyska, Color barvaVnitrku, Color barvaOkraje) {
+        this.x = x;
+        this.y = y;
+        this.sirka = sirka;
+        this.vyska = vyska;
+        this.barvaVnitrku = barvaVnitrku;
+        this.barvaOkraje = barvaOkraje;
+    }
+    public Rectangle getOkraje() {
+        return new Rectangle(x, y, sirka, vyska);
+    }
+
+    public void vykresliSe(Graphics g){
+        g.setColor(barvaVnitrku);
+        g.fillRect(x, y, sirka, vyska);
+        g.setColor(barvaOkraje);
+        g.drawRect(x, y, sirka, vyska);
+    }
 }
